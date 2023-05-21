52299ec97e8a3ae3b899853a3fc26b1e13ea4253
Trevor Oler <18trevor3695@gmail.com>
2023-05-21 17:01:20 -0400

    Update and rename well_do_you_really_need_this_file.js to main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { exec } = require("child_process");
 
 let rawdata = fs.readFileSync('this_is_literally_the_longest_name_for_a_unnecessarily_meaningless_file.json');
 let data = JSON.parse(rawdata);
@@ -7,3 +8,15 @@ let str = data['encrypted'];
 let encodedStr = Buffer.from(str).toString('base64');
 
 fs.writeFileSync('base64.json', `{"encrypted": "${encodedStr}"}`);
+
+exec("python3 your_python_script.py", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
