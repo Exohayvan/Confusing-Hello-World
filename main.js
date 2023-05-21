19cb20897216a1f1bd62a78ce603953131655038
Trevor Oler <18trevor3695@gmail.com>
2023-05-21 17:11:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 const fs = require('fs');
 const { exec } = require("child_process");
-
 let rawdata = fs.readFileSync('this_is_literally_the_longest_name_for_a_unnecessarily_meaningless_file.json');
 let data = JSON.parse(rawdata);
-
 let str = data['encrypted'];
 let encodedStr = Buffer.from(str).toString('base64');
-
 fs.writeFileSync('base64.json', `{"encrypted": "${encodedStr}"}`);
-
 exec("python3 main.py", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
