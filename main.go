f215db5d741e10942f98a6181cb206ef2e0a3358
Trevor Oler <18trevor3695@gmail.com>
2023-05-21 17:14:38 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,28 +1,21 @@
 package main
-
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "os"
 )
-
 type Decoded struct {
-	Decoded string `json:"decoded"`
+    Decoded string `json:"decoded"`
 }
-
 func main() {
-	jsonFile, err := os.Open("decoded.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	var decoded Decoded
-
-	json.Unmarshal(byteValue, &decoded)
-
-	fmt.Println(decoded.Decoded)
+    jsonFile, err := os.Open("output.json")
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer jsonFile.Close()
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+    var decoded Decodes
+    json.Unmarshal(byteValue, &decoded)
+    fmt.Println(decoded.Decoded)
 }
